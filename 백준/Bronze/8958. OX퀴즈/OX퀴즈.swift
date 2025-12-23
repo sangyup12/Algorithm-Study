9a4891e375bdf3dbfa33823ce67f046ccb89b4cf
Sangyup Lee <108389915+sangyup12@users.noreply.github.com>
2025-12-23 12:49:51 +0900

    [Bronze II] Title: OX퀴즈, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.swift
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.swift
@@ -2,12 +2,11 @@ let time = Int(readLine()!)!
 
 for _ in 0..<time {
   let answer = readLine()!
-  let arrayAnswer = String(answer).map { String($0) }
   var count = 0
   var grade = 0
   
-  for i in 0..<arrayAnswer.count {
-    if (arrayAnswer[i] == "O") {
+  for ch in answer {
+    if (ch == "O") {
       count += 1
       grade += count
     } else {
